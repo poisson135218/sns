0c7b8f0062f44d3de97f7006f8128b18c117a36e
poisson135218 <poisson0315@gmail.com>
2024-06-30 19:27:17 +0900

    デプロイ

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +27,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(){
         Paginator::useBootstrap();
     \URL::forceScheme('https');
+    
+    if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
+    
+    
 }
